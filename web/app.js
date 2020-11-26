21d847c7c4a3bd17601847d91871fbd090de2944
Jeff Shillitto <jeff@shillitto.com>
2020-11-27 08:55:13 +1100

    Fix indentation

--- web/app.js
+++ web/app.js
@@ -1,5 +1,7 @@
-var apiEndpoint = 'http://localhost:3000/demo/shotstack'; // 'https://jeh7qrmbub.execute-api.ap-southeast-2.amazonaws.com/demo/shotstack';
-var urlEndpoint = apiEndpoint + '/url';
+var apiUrl = 'http://localhost:3000/demo/'; // 'https://jeh7qrmbub.execute-api.ap-southeast-2.amazonaws.com/demo/';
+var apiEndpoint = apiUrl + 'shotstack'; // 'https://jeh7qrmbub.execute-api.ap-southeast-2.amazonaws.com/demo/shotstack';
+var urlEndpoint = apiUrl + 'upload/sign';
+var s3Bucket = 'https://shotstack-demo-storage.s3-ap-southeast-2.amazonaws.com/'
 var progress = 0;
 var progressIncrement = 10;
 var pollIntervalSeconds = 10;
@@ -15,13 +17,13 @@ function initialiseVideo(src) {
 
     player = new Plyr('#player', {
         controls: [
-        'play-large',
-        'play',
-        'progress',
-        'mute',
-        'volume',
-        'download',
-        'fullscreen'
+            'play-large',
+            'play',
+            'progress',
+            'mute',
+            'volume',
+            'download',
+            'fullscreen'
         ]
     });
 
@@ -47,7 +49,7 @@ function initialiseVideo(src) {
  * @param {String} id  the render job UUID
  */
 function pollVideoStatus(id) {
-    $.get(apiEndpoint + '/' + id, function(response) {
+    $.get(apiEndpoint + '/' + id, function (response) {
         updateStatus(response.data.status);
         if (!(response.data.status === 'done' || response.data.status === 'failed')) {
             setTimeout(function () {
@@ -119,7 +121,7 @@ function displayError(error) {
         var response = error.responseJSON;
 
         if (response.data.isJoi) {
-            $.each(response.data.details, function(index, error) {
+            $.each(response.data.details, function (index, error) {
                 if (error.context.key === 'search') {
                     $('#search-group label, #search').addClass('text-danger is-invalid');
                     $('#search-group').append('<div class="d-block invalid-feedback">Enter a subject keyword to create a video</div>').show();
@@ -196,18 +198,16 @@ function submitVideoEdit() {
         'duration': $('#clip-length').val()
     };
 
-    var s3Bucket = 'https://shotstack-demo-storage.s3-ap-southeast-2.amazonaws.com/'
-
     if (val.video == 'url') {
         formData['video'] = $('#video-url').val();
     } else {
-        formData['video'] = s3Bucket+$('#video-file .name').attr("data-file");
+        formData['video'] = s3Bucket + $('#video-file .name').attr("data-file");
     }
 
     if (val.watermark == 'url') {
         formData['watermark'] = $('#watermark-url').val();
     } else {
-        formData['watermark'] = s3Bucket+$('#watermark-file .name').attr("data-file");
+        formData['watermark'] = s3Bucket + $('#watermark-file .name').attr("data-file");
     }
 
     $.ajax({
@@ -217,15 +217,15 @@ function submitVideoEdit() {
         dataType: 'json',
         crossDomain: true,
         contentType: 'application/json'
-    }).done(function(response) {
+    }).done(function (response) {
         if (response.status !== 'success') {
             displayError(response.message);
             $('#submit-video').prop('disabled', false);
         } else {
             pollVideoStatus(response.data.response.id);
         }
-    }).fail(function(error) {
-        displayError({status: 400, });
+    }).fail(function (error) {
+        displayError({ status: 400, });
         $('#submit-video').prop('disabled', false);
     });
 }
@@ -280,12 +280,12 @@ function initialiseDownload(url) {
     $('#download').attr("href", url);
 }
 
-$(document).on('click','#advanced-checkbox',function(e){
-    
-    if($('#advanced-checkbox').is(':checked')){
+$(document).on('click', '#advanced-checkbox', function (e) {
+
+    if ($('#advanced-checkbox').is(':checked')) {
         $('#advanced').slideDown('fast');
         $('#advanced-checkbox-group .fas').attr('class', 'fas fa-caret-up float-right');
-        $(('input.advanced') && ('select.advanced')).prop('required',true);
+        $(('input.advanced') && ('select.advanced')).prop('required', true);
     } else {
         $('#advanced').slideUp('fast');
         $('#advanced-checkbox-group .fas').attr('class', 'fas fa-caret-down float-right');
@@ -294,13 +294,13 @@ $(document).on('click','#advanced-checkbox',function(e){
 
 });
 
-$(document).on('click', '.url-button', function(e){
+$(document).on('click', '.url-button', function (e) {
 
     var videoUrl = $(this).closest('.toggle').siblings('.input-url');
     var button = $(this);
     var downloadButton = $(this).closest('.toggle').find('.upload-button');
 
-    videoUrl.slideToggle('fast', function(){
+    videoUrl.slideToggle('fast', function () {
         if (videoUrl.is(':hidden')) {
             button.removeClass('btn-primary').addClass('btn-secondary');
             videoUrl.removeAttr('required');
@@ -314,20 +314,20 @@ $(document).on('click', '.url-button', function(e){
         }
     });
 
-    
+
 })
 
-$(document).on('click','.upload-button',function(e){
+$(document).on('click', '.upload-button', function (e) {
     e.preventDefault();
-    $(this).closest('.toggle').siblings('.upload').prop('required',true);
+    $(this).closest('.toggle').siblings('.upload').prop('required', true);
     $(this).closest('.toggle').siblings('.upload').click();
 });
 
-$(document).on('click','.remove-file',function(e){
+$(document).on('click', '.remove-file', function (e) {
     removeFile($(this));
 });
 
-function removeFile(thisObj){
+function removeFile(thisObj) {
     thisObj.closest('div').siblings('.name').empty();
     thisObj.closest('div').siblings('.name').removeAttr('data-file');
     thisObj.closest('.file-placeholder').addClass('d-none');
@@ -335,13 +335,13 @@ function removeFile(thisObj){
     thisObj.closest('.file-placeholder').siblings('.toggle').find('.url-button').prop('disabled', false);
 }
 
-$(document).on('change','.upload', function(e){
+$(document).on('change', '.upload', function (e) {
 
     var name = e.target.files[0].name;
     var type = e.target.files[0].type;
 
-    getPresignedPostData(name, type, function(data){
-        uploadFile(e.target.files[0],data,e.target);
+    getPresignedPostData(name, type, function (data) {
+        uploadFile(e.target.files[0], data, e.target);
     });
 
 });
@@ -352,7 +352,7 @@ function uploadFile(file, presignedPostData, thisObj) {
 
     Object.keys(presignedPostData.fields).forEach(key => {
         formData.append(key, presignedPostData.fields[key]);
-    });    
+    });
 
     formData.append('file', file);
 
@@ -365,7 +365,7 @@ function uploadFile(file, presignedPostData, thisObj) {
         data: formData,
         contentType: false,
         processData: false
-    }).done(function(response, statusText, xhr) {
+    }).done(function (response, statusText, xhr) {
         $(thisObj).siblings('.toggle').find('.loading-image').addClass('d-none');
         $(thisObj).siblings('.toggle').find('.upload-icon').removeClass('d-none');
         if (xhr.status == 204) {
@@ -377,7 +377,7 @@ function uploadFile(file, presignedPostData, thisObj) {
         } else {
             console.log(xhr.status);
         }
-    }).fail(function(error) {
+    }).fail(function (error) {
         console.log(error);
     });
 
@@ -399,25 +399,25 @@ function getPresignedPostData(name, type, callback) {
         dataType: 'json',
         crossDomain: true,
         contentType: 'application/json'
-    }).done(function(response) {
+    }).done(function (response) {
         if (response.status !== 'success') {
             displayError(response.message);
         } else {
             callback(response.data);
         }
-        
-    }).fail(function(error) {
-        displayError({status: 400, });
+
+    }).fail(function (error) {
+        displayError({ status: 400, });
     });
 
 }
 
-function validateToggleButtons(){
-    var buttonActivation = {number: 0, video: null, watermark: null};
-    $('.toggle-button').each(function(index){
+function validateToggleButtons() {
+    var buttonActivation = { number: 0, video: null, watermark: null };
+    $('.toggle-button').each(function (index) {
         var sub = $(this)[0].parentElement.id.split('-')[0];
         var type = $(this)[0].parentElement.id.split('-')[2];
-        $.map($(this)[0].classList, function(value, index){
+        $.map($(this)[0].classList, function (value, index) {
             if (value == 'btn-primary' && sub == 'video') {
                 buttonActivation.number++;
                 buttonActivation.video = type;
@@ -432,8 +432,8 @@ function validateToggleButtons(){
 
 $('[data-toggle="tooltip"]').tooltip({ trigger: 'click' });
 
-$(document).ready(function() {
-    $('form').submit(function(event) {
+$(document).ready(function () {
+    $('form').submit(function (event) {
         if (validateToggleButtons().number == 2) {
             resetErrors();
             resetVideo();
